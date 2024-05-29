cc3d75fa074d4038129b97eddebc059218ae00a2
Shraghav <harishragavan04@gmail.com>
2024-05-29 18:08:22 +0530

    Added QueryClient for datafetch

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,16 +4,26 @@ import './global.css'
 import { BrowserRouter as Router } from "react-router-dom"
 import AppRoutes from './AppRoutes'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
-
+import { QueryClient, QueryClientProvider } from 'react-query'
+//the below code is to make sure that data is not lost after we move front and back of the website
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus : false,
+    },
+  }
+})
 //main part of frontend
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      {/* all the components have access to auth Auth0Provider */}
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
-      {/* keeping code organized */}
+      <QueryClientProvider client={queryClient}>
+        {/* all the components have access to auth Auth0Provider */}
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+        {/* keeping code organized */}
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 )

9b9035ba07b3765defe21358bf5ca46145c1f15c
Shraghav <harishragavan04@gmail.com>
2024-05-31 19:16:39 +0530

    Added JWT check

--- backend/src/Routes/myUserRoutes.ts
+++ backend/src/Routes/myUserRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import myUserController from "../Controllers/myUserController";
+import { jwtCheck } from "../Middlewere/auth";
 
 const router = express.Router();
 //when comes from api/my/user and if it is post request, it will use the below
-router.post("/", myUserController.createCurrentUser);
+//athenticated backend endpoint
+router.post("/",jwtCheck, myUserController.createCurrentUser);
 
 export default router

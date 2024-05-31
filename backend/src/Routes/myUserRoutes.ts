ffba7595fdfd5442e0523699155093ec07356e18
Shraghav <harishragavan04@gmail.com>
2024-05-31 22:37:37 +0530

    Added jwtParse and validate MyUserRequest

--- backend/src/Routes/myUserRoutes.ts
+++ backend/src/Routes/myUserRoutes.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import myUserController from "../Controllers/myUserController";
-import { jwtCheck } from "../Middlewere/auth";
+import { jwtCheck, jwtParse } from '../Middlewere/auth';
+import { validateMyUserRequest } from '../Middlewere/validation';
 
 const router = express.Router();
 //when comes from api/my/user and if it is post request, it will use the below
 //athenticated backend endpoint
+// jwtcheck -> based on the  credentials when we create the account
+//jwtparse -> pick access token and get info
+//validateMyUserRequest -> making sure that there are no fields are missing
 router.post("/",jwtCheck, myUserController.createCurrentUser);
 
+//update current user (since we are creating the user here too)
+router.put("/",jwtCheck,jwtParse,validateMyUserRequest,myUserController.updateCurrentUser)
 export default router

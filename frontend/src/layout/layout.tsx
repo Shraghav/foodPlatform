808c037d13bb6862dd07199a436a64580f0f7847
Shraghav <harishragavan04@gmail.com>
2024-06-01 08:52:33 +0530

    Just added a functionality to show the hero image only in ho,me page and not in user profile page

--- frontend/src/layout/layout.tsx
+++ frontend/src/layout/layout.tsx
@@ -3,15 +3,17 @@ import Hero from "@/components/Hero";
 import Header from "@/components/header";
 
 type Props = {
-  children: React.ReactNode
+  children: React.ReactNode;
+  showHero?: boolean; //this prop is optional
 }
 
-const Layout = ({children}: Props) => {
+const Layout = ({children,showHero=false}: Props) => {
     return (
         //using flex box to make sure it takes up the entire height
         <div className="flex flex-col min-h-screen">
         <Header />
-        <Hero/>
+        {showHero && <Hero />}
+        
             {/* adding space on left and right */}
         <div className="container mx-auto flex-1 py-10">{children}</div>
         <Footer />

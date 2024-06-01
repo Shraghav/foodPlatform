4a5206216695126da05e7758bc55c58eddfd5c4f
Shraghav <harishragavan04@gmail.com>
2024-06-01 10:19:06 +0530

    Updating the user with userAPI

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,8 +1,11 @@
+import { useUpdateMyUser } from "@/API/MyUserAPI";
 import UserProfileForm from "@/Forms/user-profile/UserProfileForm";
 
 const UserProfilePage = () => {
+    //from my user api where we fetched request and returned things
+    const { updateUser, isLoading } = useUpdateMyUser();
     return (
-        <UserProfileForm />
+        <UserProfileForm onSave={updateUser} isLoading={isLoading} />
     )
 }
 

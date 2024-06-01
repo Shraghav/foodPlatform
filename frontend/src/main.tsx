38514604bbf25065e66c3edbb1cecc444060a7ea
Shraghav <harishragavan04@gmail.com>
2024-06-01 10:52:47 +0530

    Importing toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router } from "react-router-dom"
 import AppRoutes from './AppRoutes'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { Toaster } from 'sonner'
+
 //the below code is to make sure that data is not lost after we move front and back of the website
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         {/* all the components have access to auth Auth0Provider */}
         <Auth0ProviderWithNavigate>
           <AppRoutes />
+          <Toaster visibleToasts={1} position='top-right' richColors />
         </Auth0ProviderWithNavigate>
         {/* keeping code organized */}
       </QueryClientProvider>
